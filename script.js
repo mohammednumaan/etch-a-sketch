a7639e9bad446a60240070755d777dffbb8e64b8
Mohammed Numaan <138296610+mohammednumaan@users.noreply.github.com>
2023-07-13 21:34:19 +0530

    Updated

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 const container = document.querySelector(".sketch-container")
+const clearButton = document.querySelector("#clear-button")
 
 function makeGrid(val) {    
-    for (i = 1; i <= val * val; i++) {
+    for (i = 1; i <= (val * val); i++) {
       const cell = document.createElement("div");
       cell.classList.add("cell");
+      cell.style.width = 635/val - 1.6 + 'px' 
+      cell.style.height =600/val - 1.6 + 'px'
+      console.log(cell.style.width) 
+      
+
       container.appendChild(cell);
+      cell.addEventListener("mouseover", () => {
+        cell.classList.add('hover')
+      })
+
+      clearButton.addEventListener('click', () => {
+        cell.classList.remove('hover')
+    })
     }
+
+    
 
 }
 
-makeGrid(16)
+
+makeGrid(64)
 
 
   
