2357fa4ad823da059296660204dcb20f4a477e67
Mohammed Numaan <138296610+mohammednumaan@users.noreply.github.com>
2023-07-13 19:29:49 +0530

    Updated

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
 const container = document.querySelector(".sketch-container")
 
-
-for (let i = 0; i<16; i++){
-    let cell = document.createElement('div')
-    cell.classList.add('cell')
-    container.appendChild(cell)
-
-    cell.addEventListener('mouseover',() => {
-        cell.classList.add('hover')
-    })
-    
-    for (let j = 0; j<15; j++){
-        let cellV = document.createElement('div')
-        cellV.classList.add('cellV')
-        container.appendChild(cellV)
-
-        cellV.addEventListener('mouseover',() => {
-            cellV.classList.add('hover')
-        })
+function makeGrid(val) {    
+    for (i = 1; i <= val * val; i++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      container.appendChild(cell);
     }
+
 }
+
+makeGrid(16)
+
+
+  
