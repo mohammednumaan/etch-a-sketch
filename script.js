88e71e2e2e3353b003e2a33632ee8fc26a18d899
Mohammed Numaan <138296610+mohammednumaan@users.noreply.github.com>
2023-07-14 09:47:28 +0530

    Updated

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const container = document.querySelector(".sketch-container")
-const clearButton = document.querySelector("#clear-button")
+const clearButton = document.querySelector("#clear")
+const defaultButton = document.querySelector("#default")
+const rgbButton = document.querySelector("#rgb")
+const size = document.querySelector("#grid-size")
 
-function makeGrid(val) {    
+
+
+
+function makeGrid(val) {  
+  
     for (i = 1; i <= (val * val); i++) {
       const cell = document.createElement("div");
       cell.classList.add("cell");
-      cell.style.width = 635/val - 1.6 + 'px' 
-      cell.style.height =600/val - 1.6 + 'px'
+      cell.style.width = 640/val - 1.6 + 'px' 
+      cell.style.height =625/val - 1.6 + 'px'
       console.log(cell.style.width) 
       
 
@@ -15,17 +22,54 @@ function makeGrid(val) {
         cell.classList.add('hover')
       })
 
+      defaultButton.addEventListener("click", () => {
+        cell.addEventListener("mouseover", () => {
+          cell.classList.add('hover')
+          cell.style.background = null
+        })
+      })
+
+      rgbButton.addEventListener("click", () => {
+        cell.addEventListener('mouseover', () => {
+          cell.style.background = generateRandomColor()
+        })
+      })
+
       clearButton.addEventListener('click', () => {
         cell.classList.remove('hover')
+        cell.style.background = null
     })
-    }
+
+      
+
+
 
     
-
+  }
 }
 
+function generateRandomColor(){
+  let r = Math.floor(Math.random() * 256); // Random between 0-255
+  let g = Math.floor(Math.random() * 256); // Random between 0-255
+  let b = Math.floor(Math.random() * 256); // Random between 0-255
+  return 'rgb(' + r + ' , '+ g +','+ b +')'
+}
 
-makeGrid(64)
+function getGridSize(){
+  let userInput = Number(prompt('Enter Your Grid-Size (16 - 64) :'))
+  if (userInput < 16 || userInput > 64) {
+    userInput = Number(prompt('Please Enter Valid Grid-Size (16 - 64) :'))
+  }
+  return userInput
+}
+
+size.addEventListener('click', () => {
+  container.innerHTML = ""
+  grid_size = getGridSize()
+  makeGrid(grid_size)
+
+})
 
 
+makeGrid(16)
   
