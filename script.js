72201a8576f9192ba8e9d65f2e9e16a75e0d412e
Mohammed Numaan <138296610+mohammednumaan@users.noreply.github.com>
2024-02-15 11:59:56 +0530

    code: updated ui and added new features

--- script.js
+++ script.js
@@ -1,66 +1,61 @@
-const container = document.querySelector(".sketch-container")
-const clearButton = document.querySelector("#clear")
-const defaultButton = document.querySelector("#default")
-const rgbButton = document.querySelector("#rgb")
-const size = document.querySelector("#grid-size")
+const sketchBoard = document.querySelector('.sketch-board');
+const sketchContainer = document.querySelector('.sketch-container');
+const colorPicker = document.querySelector('#color-picker')
+const eraser = document.querySelector('#eraser')
+const pen = document.querySelector('#pen')
 
 
-function makeGrid(val) {  
+let inkMode = '#000000'
+
+function generateGrid(size){
+
+  for (let i = 0; i < size; i++){
+    for (let j = 0; j < size; j++){
+      const cell = document.createElement('cell');
+      cell.className = 'sketch-cell';
+      cell.style.width = (500/size) - 1.6 + 'px'
+      cell.style.height = (500/size) - 1.6 + 'px'
+      sketchBoard.appendChild(cell);
+    }
+  }
+}
+
+
+function handleEvents(){
   
-    for (i = 1; i <= (val * val); i++) {
-      const cell = document.createElement("div");
-      cell.classList.add("cell");
-      cell.style.width  = 635/val - 1.6 + 'px' 
-      cell.style.height = 600/val - 1.6 + 'px'
-  
-      
+  const highlightCallback = (event) => addColorToCell(event.target, inkMode)
 
-      container.appendChild(cell);
-      cell.addEventListener("mouseover", () => {
-        cell.classList.add('hover')
-      })
-
-      defaultButton.addEventListener("click", () => {
-        cell.addEventListener("mouseover", () => {
-          cell.classList.add('hover')
-          cell.style.background = null
-        })
-      })
-
-      rgbButton.addEventListener("click", () => {
-        cell.addEventListener('mouseover', () => {
-          cell.style.background = generateRandomColor()
-        })
-      })
-
-      clearButton.addEventListener('click', () => {
-        cell.classList.remove('hover')
-        cell.style.background = null
+  pen.addEventListener('click', (e) => {
+    
+    inkMode = colorPicker.value 
+    
+    sketchBoard.addEventListener('mousedown', (e) => {  
+      e.preventDefault()
+      addColorToCell(e.target, inkMode)
+      sketchBoard.addEventListener('mouseover', highlightCallback)
     })
-  }
+    
+    sketchBoard.addEventListener('mouseup', () => {
+      sketchBoard.removeEventListener('mouseover', highlightCallback )
+    })
+
+  })
+  
+  colorPicker.addEventListener('click', (e) => inkMode = e.target.value)
+  colorPicker.addEventListener('change', (e) => inkMode = e.target.value)
+  eraser.addEventListener('click', () => inkMode = '#FFFFFF')
+
 }
 
-function generateRandomColor(){
-  let r = Math.floor(Math.random() * 256); // Random between 0-255
-  let g = Math.floor(Math.random() * 256); // Random between 0-255
-  let b = Math.floor(Math.random() * 256); // Random between 0-255
-  return 'rgb(' + r + ' , '+ g +','+ b +')'
+
+function addColorToCell(cell, color){
+  if (!cell.classList.contains('sketch-cell')) return;
+  cell.style.backgroundColor = color;
 }
 
-function getGridSize(){
-  let userInput = Number(prompt('Enter Your Grid-Size (16 - 64) :'))
-  if (userInput < 16 || userInput > 64) {
-    getGridSize()
-  }
-  return userInput
-}
-
-size.addEventListener('click', () => {
-  container.innerHTML = ""
-  grid_size = getGridSize()
-  makeGrid(grid_size)
-
-})
 
 
-makeGrid(16)
+generateGrid(16)
+handleEvents()
+
+window.addEventListener('load', () => colorPicker.value = '#000000')
